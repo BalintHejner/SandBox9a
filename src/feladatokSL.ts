c219181c6a548993f12086e9f6b96069f6b5cc0d
BalintHejner <balinthejner204@gmail.com>
2020-05-28 18:06:50 +0200

    up

--- src/feladatokSL.ts
+++ src/feladatokSL.ts
@@ -17,9 +17,42 @@ function haromszogter(a: number, b: number, c: number): number {
 }
 
 // F3.: Írjon függvényt, amely paraméterben egy számokat tartalmazó vektort kap és visszaadja a számok átlagát!
+
+function atlag(szamok: number[]): number {
+    let sum: number = 0;
+    for (const i of szamok) {
+        sum = sum + i;
+    }
+    return sum / szamok.length;
+}
+
 // F4.: Írjon függvényt, amely paraméterben egy számokat tartalmazó vektort kap és logikai értékkel (boolean) tér vissza, amely true ha van a számok között negatív szám és false ha nincs!
+
+function negativ(vektor: number[]): boolean {
+    for (const g of vektor) {
+        if (g < 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // F5.: Írjon függvényt, amely paraméterben egy sztingeket tartalmazó vektort kap, és kiírja a vektor elemeit egymás alá!
+
+function iras(szavak: string[]): string {
+    let szting: string = "";
+    for (const h of szavak) {
+        szting = szting + h;
+    }
+    return szting;
+}
+
 // F6.: Írjon függvényt, amely paraméterben két téglalap a és b oldálát kapja, és true értékkel tér vissza ha a két téglalap hasonló, false-szal ha nem! (Két téglalap hasonló, ha az oldalaik aránya egyenlő.)
+
+function teglalap(a: number, b: number, c: number, d: number): boolean {
+    return a / b == c / d;
+}
+
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         // favicon.ico kérés kiszolgálása:
@@ -30,14 +63,14 @@ export default class Content {
         }
         // Weboldal inicializálása + head rész:
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<!DOCTYPE html>");
-        res.write("<html lang='hu'>");
-        res.write("<head>");
-        res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
-        res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-        res.write("<title>SandBox 9A</title>");
-        res.write("</head>");
-        res.write("<body><form><pre>");
+        ("<!DOCTYPE html>");
+        ("<html lang='hu'>");
+        ("<head>");
+        ("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
+        ("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+        ("<title>SandBox 9A</title>");
+        ("</head>");
+        ("<body><form><pre>");
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const params = url.parse(req.url as string, true).query;
 
@@ -47,9 +80,17 @@ export default class Content {
 
         res.write(`A háromszög területe: ${haromszogter(9, 6, 3)} + \n`);
 
+        res.write(`Átlagaik: ${atlag([245, 13, 924])} + \n`);
+
+        res.write(`Van negatív? ${negativ([-58, 5, 84])} + \n`);
+
+        res.write(`Elemek: ${iras(["Egy", "Bodza", "Megszentségteleníthetetlenségeskedéseitekért", "Korona"])} + \n`);
+
+        res.write(`Téglalap: ${teglalap}`);
+
         // <---- Fejezd be a kódolást
 
-        res.write("</pre></form></body></html>");
+        ("</pre></form></body></html>");
         res.end();
     }
 }
